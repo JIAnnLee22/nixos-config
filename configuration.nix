98446a004d0f8c2c63f0bd5c16d29277bc0e1465
JIAnnLee22 <947807614@qq.com>
2026-03-13 09:41:19 +0800

    Revert "使用flake st"
    This reverts commit 109803559a781ad74ccd1c6180a1529cf7635fcd.

--- configuration.nix
+++ configuration.nix
@@ -2,11 +2,8 @@
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
 
-{ config, lib, pkgs, st, ... }:
+{ config, lib, pkgs, ... }:
 
-let
-  stPkg = st.packages.${pkgs.system}.default;
-in
 {
   nix.gc = {
     automatic = true;
@@ -193,7 +190,14 @@ in
     cursor-cli
     claude-code
     opencode
-    stPkg
+    (st.overrideAttrs (oldAttrs: rec {
+      src = fetchFromGitHub {
+        owner = "JIAnnLee22";
+        repo = "st";
+        rev = "58258030d0e1418225b5a9fb8cd13ac08eb663fa";  # 例如："v1.0.0" 或 "a1b2c3d..."
+        sha256 = "sha256-NeOIlrj4iL2BoRNVeWx05+Zea/JSmwKcGPLqwuxhXRc=";
+      };
+    }))
   ];
   nixpkgs.overlays = [
     (final: prev: {
--- flake.nix
+++ flake.nix
@@ -2,13 +2,9 @@
   description = "NixOS flake for jiannlee22";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-
-    # 使用自己的 st 仓库 flake
-    st.url = "github:JIAnnLee22/st";
-    st.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, st, ... }:
+  outputs = { self, nixpkgs, ... }:
   let
     system = "x86_64-linux";
   in
@@ -19,10 +15,6 @@
         ./configuration.nix
         ./host/ser
       ];
-      # 把 st 这一 flake 作为 specialArgs 传给 NixOS 配置
-      specialArgs = {
-        inherit st;
-      };
     };
   };
 }
