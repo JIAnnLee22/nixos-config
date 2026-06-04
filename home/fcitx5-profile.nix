a0e2824a69399ce6aefe1637c24b522774f61630
JIAnnLee22 <947807614@qq.com>
2026-06-04 12:15:21 +0800

    update fcitx config

--- home/fcitx5-profile.nix
+++ home/fcitx5-profile.nix
@@ -8,25 +8,17 @@ in
     "Groups/0" = {
       Name = "Default";
       "Default Layout" = "us";
-      DefaultIM = "shuangpin";
+      DefaultIM = "flypy";
     };
     "Groups/0/Items/0" = {
       Name = "keyboard-us";
       Layout = "";
     };
     "Groups/0/Items/1" = {
-      Name = "shuangpin";
-      Layout = "";
-    };
-    "Groups/0/Items/2" = {
       Name = "flypy";
       Layout = "";
     };
-    "Groups/0/Items/3" = {
-      Name = "tiger";
-      Layout = "";
-    };
-    "Groups/0/Items/4" = {
+    "Groups/0/Items/2" = {
       Name = "tigress";
       Layout = "";
     };
