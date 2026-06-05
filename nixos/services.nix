c6cddc42371618e98e24371a6c788589e91f4599
JIAnnLee22 <947807614@qq.com>
2026-06-05 14:21:57 +0800

    udpate configurationLimit

--- nixos/services.nix
+++ nixos/services.nix
@@ -14,7 +14,7 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
+  boot.loader.systemd-boot.configurationLimit = 1;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
