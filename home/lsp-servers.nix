5c94a7d168c0880919e55942628963754fecb029
JIAnnLee22 <947807614@qq.com>
2026-06-25 16:25:14 +0800

    remove kotlin-lsp

--- home/lsp-servers.nix
+++ home/lsp-servers.nix
@@ -1,13 +1,11 @@
 { pkgs, ... }:
 let
-  kotlin-lsp-jetbrains = pkgs.callPackage ./kotlin-lsp.nix { };
 in
 {
   home.packages = with pkgs; [
     # LSP servers (Neovim config is managed outside Home Manager)
     jdt-language-server
     jdk17
-    kotlin-lsp-jetbrains
     kotlin-language-server
     clang-tools
     lua-language-server
--- home/nvim/lua/plugins/config.lua
+++ home/nvim/lua/plugins/config.lua
@@ -2,103 +2,89 @@ local M = {}
 
 ---@return string cmd kotlin LSP 可执行文件名
 function M.kotlin_cmd()
-	if vim.fn.executable("kotlin-lsp") == 1 then
-		return "kotlin-lsp"
-	end
-	return "kotlin-language-server"
+  return "kotlin-language-server"
 end
 
 ---@return boolean 是否使用 JetBrains kotlin-lsp
 function M.use_kotlin_lsp()
-	return M.kotlin_cmd() == "kotlin-lsp"
+  return M.kotlin_cmd() == "kotlin-lsp"
 end
 
 ---@return string kotlin LSP 配置名
 function M.kotlin_server()
-	return M.use_kotlin_lsp() and "kotlin_lsp" or "kotlin_language_server"
+  return M.use_kotlin_lsp() and "kotlin_lsp" or "kotlin_language_server"
 end
 
 M.servers = {
-	clangd = {
-		cmd = { "clangd", "--query-driver=/nix/store/**" },
-		root_markers = { "compile_commands.json", "compile_flags.txt", ".git" },
-	},
-	jdtls = {
-		cmd = { "jdtls" },
-		root_markers = {
-			"build.gradle",
-			"build.gradle.kts",
-			"pom.xml",
-			"settings.gradle",
-			"settings.gradle.kts",
-			".git",
-		},
-		settings = {
-			java = {
-				eclipse = {
-					downloadSources = true,
-				},
-				configuration = {
-					updateBuildConfiguration = "automatic",
-				},
-				format = {
-					enabled = false,
-				},
-			},
-		},
-	},
-	lua_ls = {
-		cmd = { "lua-language-server" },
-		root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-				workspace = {
-					checkThirdParty = false,
-				},
-			},
-		},
-	},
-	nil_ls = {
-		cmd = { "nil" },
-		root_markers = { "flake.nix", ".git" },
-		settings = {
-			["nil"] = {
-				nix = {
-					flake = {
-						autoArchive = true,
-					},
-				},
-			},
-		},
-	},
+  clangd = {
+    cmd = { "clangd", "--query-driver=/nix/store/**" },
+    root_markers = { "compile_commands.json", "compile_flags.txt", ".git" },
+  },
+  jdtls = {
+    cmd = { "jdtls" },
+    root_markers = {
+      "build.gradle",
+      "build.gradle.kts",
+      "pom.xml",
+      "settings.gradle",
+      "settings.gradle.kts",
+      ".git",
+    },
+    settings = {
+      java = {
+        eclipse = {
+          downloadSources = true,
+        },
+        configuration = {
+          updateBuildConfiguration = "automatic",
+        },
+        format = {
+          enabled = false,
+        },
+      },
+    },
+  },
+  lua_ls = {
+    cmd = { "lua-language-server" },
+    root_markers = { ".luarc.json", ".luarc.jsonc", ".git" },
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+        workspace = {
+          checkThirdParty = false,
+        },
+      },
+    },
+  },
+  nil_ls = {
+    cmd = { "nil" },
+    root_markers = { "flake.nix", ".git" },
+    settings = {
+      ["nil"] = {
+        nix = {
+          flake = {
+            autoArchive = true,
+          },
+        },
+      },
+    },
+  },
 }
 
 M.kotlin_overrides = {
-	kotlin_lsp = {
-		cmd = { "kotlin-lsp" },
-		root_markers = {
-			"build.gradle",
-			"build.gradle.kts",
-			"settings.gradle",
-			"settings.gradle.kts",
-			"pom.xml",
-			".git",
-		},
-	},
-	kotlin_language_server = {
-		cmd = { "kotlin-language-server" },
-		root_markers = {
-			"build.gradle",
-			"build.gradle.kts",
-			"settings.gradle",
-			"settings.gradle.kts",
-			"pom.xml",
-			".git",
-		},
-	},
+  kotlin_language_server = {
+    cmd = { "kotlin-language-server" },
+    root_markers = {
+      "build.gradle",
+      "build.gradle.kts",
+      "settings.gradle",
+      "settings.gradle.kts",
+      "pom.xml",
+      ".git",
+    },
+  },
 }
 
 return M
