00c00fe4187bae8a4f684498c04f55c427960605
JIAnnLee22 <947807614@qq.com>
2026-08-26 18:18:20 +0800

    添加一些安卓编程语言的lsp服务

--- home/lsp-servers.nix
+++ home/lsp-servers.nix
@@ -6,6 +6,10 @@ in
     # LSP servers (Neovim config is managed outside Home Manager)
     clang-tools
     lua-language-server
+    kotlin-language-server
+    jdt-language-server
+    groovy-language-server
+    lemminx
     nil
     rust-analyzer
   ];
